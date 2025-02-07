2633e4fc8c9209d1febdc31754a4548c0a19d9d4
Gazza <sgazza@post.com>
2025-02-07 12:42:50 +0100

    Initial Commit

---
+++ Samouk/SamoukApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamoukApp.swift
+//  Samouk
+//
+//  Created by Gazza on 7.2.25..
+//
+
+import SwiftUI
+
+@main
+struct SamoukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
